b0bb0362b9fd3d4572f732a4d53a98f2e54ed23d
Kasper Jordaens <kasper.jordaens@gmail.com>
2025-06-04 17:36:09 +0200

    liveplotter

--- vsk/kaos/sketch_kaos.py
+++ vsk/kaos/sketch_kaos.py
@@ -1,5 +1,6 @@
 import vsketch
 import math
+import random
 from lib.blocks import *
 
 class KaosSketch(vsketch.SketchClass):
@@ -9,7 +10,7 @@ class KaosSketch(vsketch.SketchClass):
     W = vsketch.Param(14)
     H = vsketch.Param(20)
     noiselod = vsketch.Param(1) 
-    papersize = "790mmx1720mm"
+    papersize = "750mmx1780mm"
 
     def draw(self, vsk: vsketch.Vsketch) -> None:
         # vsk.size("a3", landscape=False)
@@ -20,18 +21,28 @@ class KaosSketch(vsketch.SketchClass):
         vsk.pushMatrix() 
         vsk.translate(0,0)
         vsk.penWidth("0.5mm")
-        vsk.fill(1)
-        vsk.rect(100,200,50,50)
+        # vsk.fill(1)
+        # vsk.rect(100,200,50,50)
+
+        # subdivpattern(vsk,20,1230,250,250,3,1)
         # vsk.rect(10,0,50,50)
-        vsk.rect(600,0,50,250)
-        vsk.rect(0,1500,150,50)
-        vsk.rect(600,1500,150,150)
+        # vsk.rect(600,0,50,250)
+        # vsk.rect(0,1500,150,50)
+        # vsk.rect(600,1500,150,150)
         # self.circlegrid(vsk)                           
         # self.squaresgrid(vsk)
         # filledSquares(vsk, 10, 250, 300, 20)
         # stackSquares(vsk, 10, 205, 10, 100)
-        # blockline (vsk, 100, 10, 10, 2000, 3, 1, 1)
-        
+        # blockline (vsk, 100, 10, 10, -20, 10, 1, 1)
+        # vsk.translate(100,400)
+        x=100
+        y=100
+        for i in range(10):
+            for j in range(20):
+                x = i*50
+                y = j*100
+                vsk.line(x,y,(i+1)*50, (j+1)*100)
+                lineflower(vsk, x, y,10,j,"branch")
         # self.blocks(vsk, 100, 100, 200, 20)
         vsk.popMatrix()
         self.finalize(vsk) 
@@ -86,6 +97,29 @@ def blockline (vsk, x1, y1, x2, y2, subdiv, blockfunc, blockfuncargs):
         vsk.rectMode("corner")
         vsk.rect(x,y, 50,50)
 
+def lineflower(vsk, x,y,size,segments,ftype):
+    segs = []
+    if (ftype == "flower"):
+        for i in range(segments):
+            segs.append([x,y,x+vsk.random(0,size) - size/2, y+ vsk.random(0, size)-size/2])
+    if (ftype == "branch"):
+        dx = x
+        dy = y
+        dirx = random.choice([-1,1])
+        diry = random.choice([-1,1])
+        for i in range(segments):
+            ddx = dx + vsk.random(0,size)*dirx
+            ddy = dy + vsk.random(0,size)*diry
+            segs.append([dx,dy,ddx,ddy])
+            dx=ddx
+            dy=ddy
+
+    vsk.pushMatrix()
+    for seg in segs:
+    # vsk.translate(x,y)
+        vsk.line(seg[0],seg[1],seg[2],seg[3])
+    vsk.popMatrix()
+
 
 
 if __name__ == "__main__":
@@ -98,43 +132,3 @@ def fillsquare(vsk, xoffset,yoffset,width,height,angle,interval,outline):
         vsk.line(xoffset+x*interval,yoffset+0,xoffset+x*interval,yoffset+height )
 
 # plotter.write(fillsquare(5000,300,1000,1000,90,80,1))
-
-
-def subdivpattern(vsk,xoffset,yoffset,width,height,generations,numtypes):
-    # types = [0.60, 0.65, 0.75, 0.64, 0.80]
-    types = [1.60, 2.65, 0.75, 3.64, 0.80]
-    # zones = [[[0,0,width,height], [100,100,130,130]]]
-    zones = [ [[0,0,width,height]] ]
-    # zones = [ [[0, 0, 10000, 10000]], [[[0, 0, 8692, 6689], [0, 6689, 8692, 10000], [8692, 6689, 10000, 10000], [8692, 0, 10000, 6689]]]]
-
-    for gen in range(generations):
-        zones.append([])
-        for sq in zones[gen]:
-            #print(zones[gen])
-            #print(sq[0], sq[1])
-            # x = random.randint((sq[2]-sq[0])/2-width/10,(sq[2]-sq[0])/2+width/10)
-            # y = random.randint((sq[3]-sq[1])/2-height/10 ,(sq[3]-sq[1])/2+height/10)
-            print(sq[0],sq[2]-sq[0])
-            print(sq[1],sq[3]-sq[1])
-            # x = (sq[2]-sq[0])/2 + random.randrange(-500,500)
-            # y = (sq[3]-sq[1])/2 + random.randrange(-500,500)
-            x = vsk.random(int(sq[0]),int(sq[0]+(sq[2]-sq[0])/2))
-            y = vsk.random(int(sq[1]),int(sq[1]+(sq[3]-sq[1])/2))
-            zones[gen+1].append([sq[0],sq[1],x,y])
-            zones[gen+1].append([sq[0],y,x,sq[3]])
-            zones[gen+1].append([x,y,sq[2],sq[3]])
-            zones[gen+1].append([x,sq[1],sq[2],y])
-            # print("printing Zones")
-            # print(zones)
-    # for idx,z in enumerate(zones):
-    for idx,s in enumerate(zones[generations]):
-        if idx < 2:
-            filltype = types[1]
-        else:
-            filltype = types[int(vsk.random(0,4))]
-        fillsquare(vsk,xoffset+s[0],yoffset+s[1],s[2]-s[0],s[3]-s[1],90,filltype,0)
-        # transforms.offset(r, (s[0],s[1]))
-        # p.append(r)
-        # plotter.select_pen(idx+1)
-
-    # return p
